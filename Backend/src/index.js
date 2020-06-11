376548d0ff22307db590c23033534f092d4c24c8
Andre ALbuquerque <alalbuquerque22@gmail.com>
2020-06-11 18:34:48 -0300

    mobileApp

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require ('cors');
-
+const {errors} = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+
+
+
+
+
+
 
 
 
